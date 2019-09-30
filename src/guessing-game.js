057a047356c913305f998f118ef73a415b86dac6
Olga Zbanok <olga@zbanok.com>
2019-10-01 00:22:52 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minvalue = min;
+        this.maxvalue = max;
     }
 
     guess() {
-
+        let current = Math.ceil((this.minvalue + this.maxvalue)/2);
+        this.currentvalue = current;
+        return current;
     }
 
     lower() {
-
-    }
+        this.maxvalue = this.currentvalue;
+            }
 
     greater() {
-
+        this.minvalue = this.currentvalue;
     }
 }
 
